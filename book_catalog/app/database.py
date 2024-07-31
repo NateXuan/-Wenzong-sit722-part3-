f1c92ae41ed4c1313f871207ff802928a33767bf
NateXuan <natexuan0805@gmail.com>
2024-08-01 00:14:05 +1000

    update Task 7.2

--- book_catalog/app/database.py
+++ book_catalog/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://admin:E5OJuKRE14iotwfsvHwJeqsjaKK1eKmO@dpg-cq0nb2aju9rs73b0500g-a.oregon-postgres.render.com/part2" #os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = "postgresql://wenzong_sit722_user:opJoTHqOzqQK2kjaYKiDKGpaorF1VEpf@dpg-cqg9kdtds78s73catas0-a.oregon-postgres.render.com/wenzong_sit722" #os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
